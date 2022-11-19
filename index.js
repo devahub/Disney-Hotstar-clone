a9f81a8992ed56fe02a3d9eb5b1417adee493b34
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-11-20 00:23:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,116 @@
+// we created an array of movies which will be shown in the carousal
+
+let movies = [
+    {
+      name: "dark",
+      des:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus officia adipisci autem mollitia quidem totam.",
+      image: "images/dark.webp"
+    },
+    {
+      name: "loki",
+      des:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus officia adipisci autem mollitia quidem totam.",
+      image: "images/slider 1.png"
+    },
+    {
+      name: "ind vs eng ",
+      des:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus officia adipisci autem mollitia quidem totam.",
+      image: "images/crick.webp"
+    },
+    {
+      name: "raya and the last dragon",
+      des:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus officia adipisci autem mollitia quidem totam.",
+      image: "images/slider 4.png"
+    },
+    {
+      name: "masoom",
+      des:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus officia adipisci autem mollitia quidem totam.",
+      image: "images/maxresdefault.jpg"
+    }
+  ];
+
+  const carousel=document.querySelector('.carousel');
+  let sliders=[];
+  let slideIndex=0;
+  
+  const createSlide=()=>{
+    if(slideIndex>=movies.length){
+        slideIndex=0;
+    }
+
+    // create dom elements---->
+
+    let slide=document.createElement('div');
+    let imgElement=document.createElement('img');
+    let content=document.createElement('div');
+    let h1=document.createElement('h1');
+    let p=document.createElement('p');
+
+    // attaching all elements
+
+    imgElement.appendChild(document.createTextNode(''));
+    h1.appendChild(document.createTextNode(movies[slideIndex].name));
+    p.appendChild(document.createTextNode(movies[slideIndex].des));
+    content.appendChild(h1);
+    content.appendChild(p);
+    slide.appendChild(content);
+    slide.appendChild(imgElement);
+    carousel.appendChild(slide);
+
+    imgElement.src=movies[slideIndex].image;
+    slideIndex++
+
+    // setting elements classnames---->
+
+    slide.className="slider";
+    content.className="slide-content";
+    h1.className="movie-title";
+    p.className="movie-des";
+
+    sliders.push(slide);
+
+
+    if(sliders.length){
+        sliders[0].style.marginLeft = `calc(-${100 * (sliders.length - 2)}% - ${
+            30 * (sliders.length - 2)
+          }px)`;
+    }
+
+  }
+
+  for(let i=0;i<3;i++){
+    createSlide();
+  }
+  setInterval(()=>{
+    createSlide();
+
+  },3000);
+
+  
+  // video cards
+
+
+  const videoCards=[...document.querySelectorAll('.video-card')];
+  videoCards.forEach(item =>{
+      item.addEventListener('mouseover', ()=>{
+        let video=item.children[1];
+        video.play();
+
+
+
+      } )
+      item.addEventListener('mouseleave', ()=>{
+        let video=item.children[1];
+        video.pause();
+
+
+
+      } )
+
+
+
+  })
